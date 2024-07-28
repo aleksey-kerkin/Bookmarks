6057500d8e3382569c69711ac24f254a22503f62
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-07-29 01:47:40 +0500

    Fixed code

--- images/forms.py
+++ images/forms.py
@@ -9,7 +9,7 @@ from .models import Image
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ["title", "urls", "description"]
+        fields = ["title", "url", "description"]
         widgets = {
             "url": forms.HiddenInput,
         }
